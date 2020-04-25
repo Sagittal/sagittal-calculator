bfdd2c8c03c5e1917cf76e6cffdd943224306236
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-04-24 17:25:52 -0700

    do not snap diacritics to wonky grid

--- fixing_bravura_sagittals_fontforge_script.py
+++ fixing_bravura_sagittals_fontforge_script.py
@@ -33,6 +33,15 @@ wonky_sagittal_grid_positions_in_ps_fu = list(map(round_ideal_to_real, grid_rang
 #	750,	766,	781,	797,	812,	828,	844,	859,	875,	891,	906,	922,	938,	953,	969,	984,
 #	1000	]
 
+# Kerning stuff
+
+glyphs_for_diacritics_to_kern_with = ['uniE3EA', 'uniE3E8', 'uniE3CE', 'uniE3CC', 'uniE3C2', 'uniE3BA', 'uniE3B8',
+'uniE39E', 'uniE39C', 'uniE392', 'uniE386', 'uniE382', 'uniE380', 'uniE37A', 'uniE374', 'uniE362',
+'uniE35E', 'uniE356', 'uniE34E', 'uniE34A', 'uniE342', 'uniE334', 'uniE332', 'uniE330', 'uniE32E',
+'uniE326', 'uniE324', 'uniE31E', 'uniE318', 'uniE316', 'uniE30A', 'uniE308', 'uniE302']
+
+kerning_table = "'kern' Horizontal Kerning lookup 0 subtable"
+
 # Utilities
 
 def find_nearest(lst, K):
@@ -67,7 +76,7 @@ def snap_point(point):
 		transformation_matrix = psMat.translate(0, nearest_y - point.y)
 		point.transform(transformation_matrix)
 
-def replace_downward_glyph_with_mirrored_snapped_upward_glyph(glyph, downward_version_of_glyph, layer):
+def replace_downward_glyph_with_mirrored_upward_glyph(glyph, downward_version_of_glyph, layer):
 	downward_version_of_glyph.activeLayer = layer
 	contours = glyph.layers[layer]
 	pen = downward_version_of_glyph.glyphPen()
@@ -81,22 +90,28 @@ def replace_downward_glyph_with_mirrored_snapped_upward_glyph(glyph, downward_ve
 
 	pen = None
 
-def fix_side_bearing(glyph, unicode):
+def fix_symbol_side_bearing(glyph, unicode):
 	glyph.condenseExtend(1, 0, 0, 0)  # remove existing side-bearing
-	if unicode >= 0xe3f2: # diacritics
-		glyph.right_side_bearing = 62 # 1/16 em
-	else:
-		glyph.right_side_bearing = 0
+	glyph.right_side_bearing = 0
+
+def fix_diacritic_side_bearing(glyph, unicode):
+	glyph.right_side_bearing = 62 # 1/16 em
 
 # Fix up the font!
 
 bravura = fontforge.open("C:/Users/DouglasBlumeyer/Desktop/BravuraSagittalUpdate.sfd")
 
-sagittal_unicode_range_start = 0xe300
-sagittal_unicode_range_end = 0xe40c
+sagittal_symbol_unicode_range_start = 0xe300
+sagittal_symbol_unicode_range_end = 0xe3f4
+
+sagittal_diacritic_unicode_range_start = 0xe3f2
+sagittal_diacritic_unicode_range_end = 0xe40c
+
+sagittal_compatibles = [0xe284, 0xe285, 0xe47b, 0xe47c, 0xe47d]
+
 skip_every_other = 2
 
-for unicode in range(sagittal_unicode_range_start, sagittal_unicode_range_end, skip_every_other):
+for unicode in range(sagittal_symbol_unicode_range_start, sagittal_symbol_unicode_range_end, skip_every_other):
 	try:
 		glyph = bravura[unicode]
 	except TypeError:
@@ -106,42 +121,30 @@ for unicode in range(sagittal_unicode_range_start, sagittal_unicode_range_end, s
 	snap_glyph_to_wonky_grid(glyph, "Fore")
 	snap_glyph_to_wonky_grid(glyph, "Back")
 
-	fix_side_bearing(glyph, unicode)
+	fix_symbol_side_bearing(glyph, unicode)
 
 	downward_version_of_glyph_unicode = unicode + 1
 	downward_version_of_glyph = bravura[downward_version_of_glyph_unicode]
-	replace_downward_glyph_with_mirrored_snapped_upward_glyph(glyph, downward_version_of_glyph, "Fore")
-	replace_downward_glyph_with_mirrored_snapped_upward_glyph(glyph, downward_version_of_glyph, "Back")
+	replace_downward_glyph_with_mirrored_upward_glyph(glyph, downward_version_of_glyph, "Fore")
+	replace_downward_glyph_with_mirrored_upward_glyph(glyph, downward_version_of_glyph, "Back")
 
-	fix_side_bearing(downward_version_of_glyph, downward_version_of_glyph_unicode)
+	fix_symbol_side_bearing(downward_version_of_glyph, downward_version_of_glyph_unicode)
 
-
-for unicode in [0xe284, 0xe285, 0xe47b, 0xe47c, 0xe47d]: # sagittal_compatibles
+for unicode in range(sagittal_diacritic_unicode_range_start, sagittal_diacritic_unicode_range_end, skip_every_other):
 	glyph = bravura[unicode]
 	print(hex(unicode))
-	snap_glyph_to_wonky_grid(glyph, "Fore")
-	snap_glyph_to_wonky_grid(glyph, "Back")
-	fix_side_bearing(glyph, unicode)
 
+	for kerning_glyph in glyphs_for_diacritics_to_kern_with:
+		glyph.addPosSub(kerning_table, kerning_glyph, 0, 0, -62, 0, 0, 0, 0, 0)
 
-# Add kerning between diacritics and symbols
+	fix_diacritic_side_bearing(glyph, unicode)
 
-glyphs_to_kern_with = ['uniE3EA', 'uniE3E8', 'uniE3CE', 'uniE3CC', 'uniE3C2', 'uniE3BA', 'uniE3B8',
-'uniE39E', 'uniE39C', 'uniE392', 'uniE386', 'uniE382', 'uniE380', 'uniE37A', 'uniE374', 'uniE362',
-'uniE35E', 'uniE356', 'uniE34E', 'uniE34A', 'uniE342', 'uniE334', 'uniE332', 'uniE330', 'uniE32E',
-'uniE326', 'uniE324', 'uniE31E', 'uniE318', 'uniE316', 'uniE30A', 'uniE308', 'uniE302']
+	downward_version_of_glyph_unicode = unicode + 1
+	downward_version_of_glyph = bravura[downward_version_of_glyph_unicode]
+	replace_downward_glyph_with_mirrored_upward_glyph(glyph, downward_version_of_glyph, "Fore")
+	replace_downward_glyph_with_mirrored_upward_glyph(glyph, downward_version_of_glyph, "Back")
 
-sagittal_diacritic_unicode_range_start = 0xe3f2
-sagittal_unicode_range_end = 0xe40c
-
-table = "'kern' Horizontal Kerning lookup 0 subtable"
-
-for unicode in range(sagittal_diacritic_unicode_range_start, sagittal_unicode_range_end):
-	diacritic_glyph = bravura[unicode]
-	print(hex(unicode))
-	for kerning_glyph in glyphs_to_kern_with:
-		diacritic_glyph.addPosSub(table, kerning_glyph, 0, 0, -62, 0, 0, 0, 0, 0)
-
+	fix_diacritic_side_bearing(downward_version_of_glyph, downward_version_of_glyph_unicode)
 
 # Add kerning between half-tinas and other tina diacritics (and minas)
 
@@ -149,13 +152,20 @@ upward_half_tina_glyph = bravura[0xe40a]
 upward_tina_glyphs_to_kern_with = ['uniE3F4', 'uniE3F6', 'uniE3F8', 'uniE3FA',
 'uniE3FC', 'uniE3FE', 'uniE400', 'uniE402', 'uniE404', 'uniE406', 'uniE408']
 for upward_tina_glyph in upward_tina_glyphs_to_kern_with:
-	upward_half_tina_glyph.addPosSub(table, upward_tina_glyph, 0, 0, -30, 0, 0, 0, 0, 0)
+	upward_half_tina_glyph.addPosSub(kerning_table, upward_tina_glyph, 0, 0, -30, 0, 0, 0, 0, 0)
 
 downward_half_tina_glyph = bravura[0xe40b]
 downward_tina_glyphs_to_kern_with = ['uniE3F5', 'uniE3F7', 'uniE3F9', 'uniE3FB',
 'uniE3FD', 'uniE3FF', 'uniE401', 'uniE403', 'uniE405', 'uniE407', 'uniE409']
 for downward_tina_glyph in downward_tina_glyphs_to_kern_with:
-	downward_half_tina_glyph.addPosSub(table, downward_tina_glyph, 0, 0, -30, 0, 0, 0, 0, 0)
+	downward_half_tina_glyph.addPosSub(kerning_table, downward_tina_glyph, 0, 0, -30, 0, 0, 0, 0, 0)
+
+for unicode in sagittal_compatibles:
+	glyph = bravura[unicode]
+	print(hex(unicode))
+	snap_glyph_to_wonky_grid(glyph, "Fore")
+	snap_glyph_to_wonky_grid(glyph, "Back")
+	fix_symbol_side_bearing(glyph, unicode)
 
 
 bravura.save()
