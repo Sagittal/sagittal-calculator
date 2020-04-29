35e0af3b5a1a0a3f89ec8708103ffdd7368c346e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-04-29 14:34:49 -0700

    sort things into directories

--- get_kerning_table.py
+++
@@ -1,3 +0,0 @@
-bravura = fontforge.open("C:/Users/DouglasBlumeyer/Desktop/BravuraSagittalUpdate.sfd")
-source_glyph = bravura[0xe3f3]
-print(source_glyph.getPosSub("'kern' Horizontal Kerning lookup 0 subtable"))
