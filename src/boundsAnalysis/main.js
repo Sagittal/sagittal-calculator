51ef3adc7a6fb47d09799f593573e79347f66639
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-02 20:57:39 -0700

    refactor main to allow for a summary result

--- src/boundsAnalysis/main.js
+++ src/boundsAnalysis/main.js
@@ -4,21 +4,24 @@ const {processHistories, HEADER_ROW} = require("./analyzeHistories/processHistor
 
 const args = process.argv.slice(2)
 
-let data
-let summary
 if (args.length) {
-    const specificDatum = args[0]
-    data = [DATA[specificDatum]]
-    summary = false
-} else {
-    data = DATA
-    summary = true
-    console.log(HEADER_ROW)
-}
+    const datumIndex = args[0]
+    const datum = DATA[datumIndex]
 
-data.map((datum, datumIndex) => {
     const histories = calculateBoundHistories(datum.bound)
-    const processedHistories = processHistories(histories, datum, datumIndex, {summary})
-
+    const processedHistories = processHistories(histories, datum, datumIndex, {summary: false})
     console.log(processedHistories)
-})
+} else {
+    console.log(HEADER_ROW)
+
+    let totalPossible = 0
+    DATA.map((datum, datumIndex) => {
+        const histories = calculateBoundHistories(datum.bound)
+        const processedHistories = processHistories(histories, datum, datumIndex, {summary: true})
+        console.log(processedHistories)
+
+        if (processedHistories.includes(true)) totalPossible += 1
+    })
+
+    console.log(`\nFor ${totalPossible} out of 149 bounds, it is possible that they were set by working up through each level they appear in, each time changing no further than by snapping to any of the three irrational bound types between the commas they bound at that level.`)
+}
