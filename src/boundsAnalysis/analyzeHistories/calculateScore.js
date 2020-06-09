d89029ef566a8fa277dccd8ae97ec32324f75b1f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-08 17:03:25 -0700

    better name for score stuff

--- src/boundsAnalysis/analyzeHistories/calculateScore.js
+++ src/boundsAnalysis/analyzeHistories/calculateScore.js
@@ -4,25 +4,29 @@ const {RANKS} = require("../data/ranks")
 const ZERO_INDEX_OFFSET = 1
 const OFFSET_FOR_INITIAL_RANK = 1
 
-const calculateScoreIndex = (rank, levelIndex) => {
+const calculateBinaryScoreRepresentationIndex = (rank, levelIndex) => {
     return (rank - OFFSET_FOR_INITIAL_RANK) * LEVELS.length + (LEVELS.length - ZERO_INDEX_OFFSET - levelIndex)
 }
 
 const calculateScore = analyzedHistories => {
-    const scoreThing = [...Array((Object.keys(RANKS).length - OFFSET_FOR_INITIAL_RANK) * LEVELS.length).keys()].map(n => 0)
+    const binaryScoreRepresentationCardinality = (Object.keys(RANKS).length - OFFSET_FOR_INITIAL_RANK) * LEVELS.length
+    const binaryScoreRepresentation = [...Array(binaryScoreRepresentationCardinality).keys()].map(_ => 0)
 
     analyzedHistories.forEach(analyzedHistory => {
-        const scoreThingIndex = calculateScoreIndex(analyzedHistory.rank, LEVELS.indexOf(analyzedHistory.level))
+        const binaryScoreRepresentationIndex = calculateBinaryScoreRepresentationIndex(analyzedHistory.rank, LEVELS.indexOf(analyzedHistory.level))
 
-        scoreThing[scoreThingIndex] += 1
+        binaryScoreRepresentation[binaryScoreRepresentationIndex] += 1
     })
 
-    return scoreThing.reduce((score, scoreThingElement, index) => {
-        return score + scoreThingElement * Math.pow(2, index)
-    }, 0)
+    return binaryScoreRepresentation.reduce(
+        (score, binaryScoreRepresentationTerm, index) => {
+            return score + binaryScoreRepresentationTerm * Math.pow(2, index)
+        },
+        0,
+    )
 }
 
 module.exports = {
     calculateScore,
-    calculateScoreIndex,
+    calculateBinaryScoreRepresentationIndex,
 }
