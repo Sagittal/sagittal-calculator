014a5cba1c77693a5a2105341e73cc7335ce148d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-11 09:26:10 -0700

    investigate > half-step of level EDA issues

--- src/boundsAnalysis/analyze/bound.js
+++ src/boundsAnalysis/analyze/bound.js
@@ -5,6 +5,7 @@ const {TINA} = require("../data/intervals")
 const rankAnalysis = require("./ranks")
 const levelAnalysis = require("./levels")
 const {computeInitialPosition} = require("../data/initialPosition")
+// const {LEVEL_EDA_STEP_SIZES} = require("../data/intervals")
 
 const analyzeBound = (histories, bound, boundIndex) => {
     const initialPosition = computeInitialPosition(bound)
@@ -16,7 +17,14 @@ const analyzeBound = (histories, bound, boundIndex) => {
     const bestRank = bestPossibleHistory.rank
     const initialPositionTinaDifference = (bound.position - initialPosition) / TINA
 
-    // console.log("BPH???", bestPossibleHistory)
+    // TODO: deal with > half-step of level EDA issues
+    // bestPossibleHistory.events.forEach((event, index) => {
+    //     if (index === 0) return
+    //     const previousEvent = bestPossibleHistory.events[index - 1]
+    //     const positionChange = event.position - previousEvent.position
+    //     const edaChange = positionChange / LEVEL_EDA_STEP_SIZES[event.level]
+    //     console.log(Math.abs(edaChange).toPrecision(5), event.level)
+    // })
 
     rankAnalysis.updateRankAnalysis(bestRank, boundIndex)
     levelAnalysis.updateLevelAnalysis(bestPossibleHistory)
