6218b4c5fb8d313b7588b1b3bace0766ccde0495
Matt Stjernholm <douglas.blumeyer@gmail.com>
2020-06-21 15:45:33 -0700

    deal with apotome slope

--- src/findTinaPrimaryCommas/main.js
+++ src/findTinaPrimaryCommas/main.js
@@ -6,9 +6,10 @@ const args = process.argv.slice(2)
 const lowerBound = args[0]
 const upperBound = args[1]
 
+// TODO: perhaps the rest of these should also be received as flags from the command line
 const MAXIMUM_SUM_OF_PRIMES_GREATER_THAN_THREE = 61
-const MAXIMUM_COUNT_OF_PRIMES_GREATER_THAN_THREE = 5
-const MAXIMUM_APOTOME_SLOPE = 15 // TODO: where did this number come from? my post? http://forum.sagittal.org/viewtopic.php?p=1661#p1661
+const MAXIMUM_COUNT_OF_PRIMES_GREATER_THAN_THREE = 555 // a silly number, unlikely to come close
+const MAXIMUM_APOTOME_SLOPE = 14
 const MAXIMUM_PRIME_LIMIT = 47
 
 let commas = []
@@ -16,13 +17,22 @@ let commas = []
 const fiveMonzosToCheck = computeFiveMonzosToCheck({
     maximumPrimeLimit: MAXIMUM_PRIME_LIMIT,
     maximumSopfgtt: MAXIMUM_SUM_OF_PRIMES_GREATER_THAN_THREE,
-    // maximumCopfgtt: MAXIMUM_COUNT_OF_PRIMES_GREATER_THAN_THREE,
+    maximumCopfgtt: MAXIMUM_COUNT_OF_PRIMES_GREATER_THAN_THREE,
 })
 
 fiveMonzosToCheck.forEach(fiveMonzoToCheck => {
-    commas = commas.concat(computeCommasFromFiveMonzo(fiveMonzoToCheck, {lowerBound, upperBound}))
+    commas = commas.concat(
+        computeCommasFromFiveMonzo(
+            fiveMonzoToCheck,
+            {
+                lowerBound,
+                upperBound,
+                maximumApotomeSlope: MAXIMUM_APOTOME_SLOPE
+            }
+        )
+    )
 })
 
 commas.sort(({sopfgtt}, {sopfgtt: nextSopfgtt}) => sopfgtt - nextSopfgtt)
 
-commas.forEach(({ formattedOutput }) => console.log(formattedOutput))
+commas.forEach(({formattedOutput}) => console.log(formattedOutput))
