d3a0714233444b3d3a75177db1cf3e13fc65927b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-24 16:38:02 -0700

    adjust display of commas for those two that are super close in the Extreme level

--- src/scripts/analyzeBounds/visualize/levelCommas.js
+++ src/scripts/analyzeBounds/visualize/levelCommas.js
@@ -19,10 +19,12 @@ const visualizeLevelCommas = () => {
 
             const positionX = computeX(position)
 
+            const adjustedUnicode = ascii === "/|~" ? unicode + "         " : ascii === ",,(|(" ? "         " + unicode : unicode
+
             levelCommaElements.push(`  <circle stroke="black" cx="${positionX}" cy="${dotY}" r="${DOT_SIZE}" />\n`)
             levelCommaElements.push(`  <text fill="white" text-anchor="middle" x="${positionX}" y="${symbolY}" font-size="10px" font-family="Helvetica">${ascii}</text>\n`) // for searchability by ascii
             // levelCommaElements.push(`  <text stroke="white" stroke-width="0.45em" text-anchor="middle" x="${positionX}" y="${symbolY}" font-size="40px" font-family="Bravura">${unicode}</text>\n`)
-            levelCommaElements.push(`  <text fill="black" text-anchor="middle" x="${positionX}" y="${symbolY}" font-size="40px" font-family="Bravura">${unicode}</text>\n`)
+            levelCommaElements.push(`  <text fill="black" text-anchor="middle" x="${positionX}" y="${symbolY}" font-size="40px" font-family="Bravura">${adjustedUnicode}</text>\n`)
 
             if (level === "EXTREME") {
                 const minaY = symbolY - MINA_OFFSET
