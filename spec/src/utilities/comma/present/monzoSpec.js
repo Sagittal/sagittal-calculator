64c7e36c712c5171c07563fbd2462d829f847ba7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-24 21:10:13 -0700

    formatted monzos with punctuation

--- spec/src/utilities/comma/present/monzoSpec.js
+++ spec/src/utilities/comma/present/monzoSpec.js
@@ -8,4 +8,12 @@ describe("presentMonzo", () => {
 
         expect(result).toBe(`[-8 -6 3 5 -1⟩`)
     })
+
+    it("can format it using George Secor's punctuated format", () => {
+        const monzo = [-8, -6, 3, 5, -1, 0, 0, 0, 5, 4, 2, 3]
+
+        const result = presentMonzo(monzo, {punctuated: true})
+
+        expect(result).toBe(`[-8 -6, 3 5 -1, 0 0 0, 5 4 2, 3⟩`)
+    })
 })
--- src/utilities/comma/present/monzo.js
+++ src/utilities/comma/present/monzo.js
@@ -1,5 +1,26 @@
-const presentMonzo = monzo => {
-    return `[${monzo.join(" ")}⟩`
+const presentMonzo = (monzo, {punctuated = false} = {}) => {
+    let contents
+    if (punctuated) {
+        const fiveRoughMonzo = monzo.splice(2)
+        contents = monzo.join(" ") + ", "
+
+        let index = 0
+        while (index < fiveRoughMonzo.length) {
+            contents = contents + fiveRoughMonzo[index]
+            if (index < fiveRoughMonzo.length - 1) {
+                if (index % 3 === 2) {
+                    contents = contents + ", "
+                } else {
+                    contents = contents + " "
+                }
+            }
+            index += 1
+        }
+    } else {
+        contents = monzo.join(" ")
+    }
+
+    return `[${contents}⟩`
 }
 
 module.exports = {
