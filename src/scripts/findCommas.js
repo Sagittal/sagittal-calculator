0ad085d38090cc176f68a8686ee1f608e998da08
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-06-24 21:17:34 -0700

    find commas script can sort

--- src/scripts/findCommas.js
+++ src/scripts/findCommas.js
@@ -11,10 +11,11 @@ program
     .option("-u, --upper-bound <upperBound>", "small pizza size", parseFloat)
     .option("-p, --prime-limit <primeLimit>", "maximum prime limit", parseInt)
     .option("-a, --apotome-slope <apotomeSlope>", "maximum absolute apotome slope", parseFloat)
-    .option("-s, --sopfgtt <sopfgtt>", "maximum sopfgtt", parseInt)
-    .option("-c, --copfgtt <copfgtt>", "maximum copfgtt", parseInt)
+    .option("-+, --sopfgtt <sopfgtt>", "maximum sopfgtt", parseInt)
+    .option("-#, --copfgtt <copfgtt>", "maximum copfgtt", parseInt)
     .option("-3, --absolute-three-exponent <absoluteThreeExponent>", "maximum absolute 3 exponent", parseInt)
     .option("-f, --five-rough-monzo <fiveRoughMonzo>", "five-rough monzo", JSON.parse)
+    .option("-s, --sort <sort>", "sort by")
     .parse(process.argv)
 
 const lowerBound = program.lowerBound || 0
@@ -25,6 +26,7 @@ const maximumApotomeSlope = program.apotomeSlope || 14
 const maximumPrimeLimit = program.primeLimit || 47
 const maximumAbsoluteThreeExponent = program.absoluteThreeExponent || 15
 const fiveRoughMonzo = program.fiveRoughMonzo
+const sort = program.sort
 
 let commas = []
 
@@ -48,7 +50,11 @@ fiveMonzosToCheck.forEach(fiveMonzoToCheck => {
     )
 })
 
-commas.sort(({sopfgtt}, {sopfgtt: nextSopfgtt}) => sopfgtt - nextSopfgtt)
+if (sort) {
+    commas.sort((a, b) => {
+        return a[sort] - b[sort]
+    })
+}
 
 console.log(FIND_COMMAS_HEADER_ROW)
 commas.forEach(comma => console.log(presentComma(comma)))
