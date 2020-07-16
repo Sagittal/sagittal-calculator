229c4aa3266ae4b1839462422b04099f6eee5cbe
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-07-15 22:56:07 -0700

    command tests fail when there is an error inside them, as they should have been doing

--- spec/helpers/specHelper.ts
+++ spec/helpers/specHelper.ts
@@ -6,7 +6,7 @@ const SKIP_THE_FINAL_EMPTY_LINE = 1
 
 const runCommandAndGetConsoleOutput: (command: string) => string[] =
     (command: string): string[] => {
-        const consoleOutput: string = cp.execSync(command, { stdio: "pipe" }).toString()
+        const consoleOutput: string = cp.execSync(command, { stdio: ["pipe", "pipe", "inherit"] }).toString()
         const consoleOutputLines: string[] = consoleOutput.split("\n")
 
         return consoleOutputLines.slice(NPM_SCRIPT_HEADER_LINES_COUNT, consoleOutputLines.length - SKIP_THE_FINAL_EMPTY_LINE)
