4c233089bc3e64be94668925a1eb5442876a8e18
Douglas Blumeyer <douglas.blumeyer@omadahealth.com>
2020-08-10 11:44:17 -0700

    TEMP reduce resolution

--- src/scripts/unpopularityMetric/bestMetric/scopeToSamples/constants.ts
+++ src/scripts/unpopularityMetric/bestMetric/scopeToSamples/constants.ts
@@ -1,7 +1,7 @@
 import { Unit } from "../../../../general"
 import { ParameterValue } from "../../sumOfSquares"
 
-const MAXIMUM_UNIT: Unit<ParameterValue> = 0.1 as Unit<ParameterValue>
+const MAXIMUM_UNIT: Unit<ParameterValue> = 0.333 as Unit<ParameterValue>
 
 export {
     MAXIMUM_UNIT,
