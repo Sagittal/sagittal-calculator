04836bd83b3bf4249c3c5c1c0b93b1a7fbcf3f4f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-08-12 22:50:22 -0700

    support running unpopularity scripts without existing dist folder

--- src/scripts/unpopularityMetric/debug/clear.ts
+++ src/scripts/unpopularityMetric/debug/clear.ts
@@ -2,7 +2,11 @@ import * as fs from "fs"
 import * as path from "path"
 
 const clearDebugLogFiles = () => {
+    if (!fs.existsSync("dist")) return
+
     const directory = "dist/unpopularityMetric"
+
+    if (!fs.existsSync(directory)) return
 
     const files = fs.readdirSync(directory)
 
