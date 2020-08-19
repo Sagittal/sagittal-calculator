9fd2f592e4cb17d672999fce58674905e88293e0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-08-18 19:38:28 -0700

    fix issue with negative zeroes in inverted monzos

--- spec/src/general/music/invertMonzoSpec.ts
+++ spec/src/general/music/invertMonzoSpec.ts
@@ -2,10 +2,10 @@ import { invertMonzo, Monzo } from "../../../../src/general/music"
 
 describe("invertMonzo", () => {
     it("returns the inverted (negated) version of the monzo", () => {
-        const monzo = [4, -3, -1, 1] as Monzo
+        const monzo = [4, 0, -1, 1] as Monzo
 
         const result = invertMonzo(monzo)
 
-        expect(result).toEqual([-4, 3, 1, -1] as Monzo)
+        expect(result).toEqual([-4, 0, 1, -1] as Monzo)
     })
 })
--- src/general/music/invertMonzo.ts
+++ src/general/music/invertMonzo.ts
@@ -3,7 +3,7 @@ import { Prime } from "../types"
 import { Monzo } from "./types"
 
 const invertMonzo = <MonzoType extends Monzo>(monzo: MonzoType): MonzoType =>
-    monzo.map(primeExponent => -primeExponent as Exponent<Prime>) as MonzoType
+    monzo.map(primeExponent => primeExponent === 0 ? 0 : -primeExponent as Exponent<Prime>) as MonzoType
 
 export {
     invertMonzo,
