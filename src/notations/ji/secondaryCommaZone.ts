01415167c3edea6c8792cb4a7941df83e0a4d919
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-08-26 20:02:44 -0700

    improve secondary comma zone

--- src/notations/ji/secondaryCommaZone.ts
+++ src/notations/ji/secondaryCommaZone.ts
@@ -4,9 +4,12 @@ import { JiSymbol, SecondaryCommaZone } from "./types"
 
 const computeSecondaryCommaZone = (symbol: JiSymbol): SecondaryCommaZone => {
     const levelBounds = LEVELS_BOUNDS[ symbol.introducingLevel ]
-    const maxCentsIndex = levelBounds.findIndex(bound => bound.cents > symbol.primaryComma.cents)
-    const maxCents = levelBounds[ maxCentsIndex ].cents
-    const minCents = maxCentsIndex === 0 ? 0 as Cents : levelBounds[ maxCentsIndex - 1 ].cents // TODO: or should it be -maxCents?
+
+    const indexOfBoundJustAboveSymbolAtThisLevel = levelBounds.findIndex(bound => bound.cents > symbol.primaryComma.cents)
+    const indexOfBoundJustBelowSymbolAtThisLevel = indexOfBoundJustAboveSymbolAtThisLevel - 1
+
+    const minCents = levelBounds[ indexOfBoundJustBelowSymbolAtThisLevel ]?.cents || 0
+    const maxCents = levelBounds[ indexOfBoundJustAboveSymbolAtThisLevel ].cents
 
     return [minCents as Min<Cents>, maxCents as Max<Cents>]
 }
