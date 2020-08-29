d787fc8d5094f4469e555e4e119b330c4ee4b0cf
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-08-29 10:22:04 -0700

    analyze-bounds functions as a snapshot test, at least for the terminal output

--- spec/src/scripts/analyzeBounds/commands/analyzeBoundsSpec.ts
+++ spec/src/scripts/analyzeBounds/commands/analyzeBoundsSpec.ts
@@ -1,10 +1,19 @@
-import * as cp from "child_process"
+import * as fs from "fs"
+import { IO } from "../../../../../src/general"
 import { onlyRunInCi } from "../../../../helpers/onlyRunInCi"
+import { runCommandAndGetConsoleOutput } from "../../../../helpers/src/scripts/runCommand"
 
 describe("analyze-bounds", () => {
     it("runs without error", () => {
         onlyRunInCi()
 
-        cp.execSync("npm run analyze-bounds -- --no-color --no-write")
+        const actual = runCommandAndGetConsoleOutput("npm run analyze-bounds -- --no-color --no-write" as IO)
+
+        const expected = fs.readFileSync(
+            "src/scripts/analyzeBounds/results/boundsTerminal.txt",
+            { encoding: "utf8" },
+        ).split("\n") as IO[]
+        expected.pop()
+        expect(actual).toEqual(expected)
     })
 })
