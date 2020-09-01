de26f1f05b6003c74861310deb89ceed708c2c25
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-01 11:25:23 -0700

    another half-apotome mirror spec

--- spec/src/sagittal/notations/ji/verificationSpecs/halfApotomeMirrorSpec.ts
+++ spec/src/sagittal/notations/ji/verificationSpecs/halfApotomeMirrorSpec.ts
@@ -1,4 +1,4 @@
-import { getSagittalComma, JI_SYMBOLS } from "../../../../../../src/sagittal"
+import { getSagittalComma, JI_BOUNDS, JI_SYMBOLS } from "../../../../../../src/sagittal"
 import { APOTOME_CENTS } from "../../../../../../src/sagittal/constants"
 
 describe("half-apotome mirror", () => {
@@ -18,6 +18,24 @@ describe("half-apotome mirror", () => {
             const mirroredComma = sagittalJiCommas[ mirroredIndex ]
 
             expect(comma.cents - halfApotomeCents).toBeCloseTo(halfApotomeCents - mirroredComma.cents)
+
+            indexOffset = indexOffset + 1
+        }
+    })
+
+    it("is the case that the bounds in the JI notation are symmetrical about the half-apotome mirror", () => {
+        const boundAtHalfApotomeMirrorIndex =
+            JI_BOUNDS.findIndex(jiBound => jiBound.cents > halfApotomeCents)
+
+        let indexOffset = 1
+        while (boundAtHalfApotomeMirrorIndex + indexOffset < JI_BOUNDS.length) {
+            const index = boundAtHalfApotomeMirrorIndex + indexOffset
+            const mirroredIndex = boundAtHalfApotomeMirrorIndex - indexOffset
+
+            const bound = JI_BOUNDS[ index ]
+            const mirroredBound = JI_BOUNDS[ mirroredIndex ]
+
+            expect(bound.cents - halfApotomeCents).toBeCloseTo(halfApotomeCents - mirroredBound.cents)
 
             indexOffset = indexOffset + 1
         }
