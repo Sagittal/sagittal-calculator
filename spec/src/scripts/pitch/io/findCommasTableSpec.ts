8325277daaa4f0071f6b54f19a5f2cdb78e2a49e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-06 13:43:02 -0700

    fix ci as usu

--- spec/src/scripts/pitch/io/findCommasTableSpec.ts
+++ spec/src/scripts/pitch/io/findCommasTableSpec.ts
@@ -34,9 +34,9 @@ describe("computeFindCommasTable", () => {
 
         const expected =
             // tslint:disable:max-line-length
-            "symbol  \tname  \tratio\tmonzo              \tcents  \tapotome slope\tlimit  \t5-rough sopfr\tN2D3P9 ".underline + NEWLINE +
-            "    /|\\ \t11M   \t33/32\t[   0   0   1 ⟩    \t 45.450\t -4.000      \t 11    \t 11          \t  6.722" + NEWLINE +
-            "        \t25/49M\t50/49\t[   1   0   2  -2 ⟩\t 33.400\t -2.154      \t  7    \t 24          \t 26.466" + NEWLINE as Io
+            "symbol  \tname  \tratio\tmonzo              \tcents  \tapotome slope\tlimit  \t5-rough sopfr\t2,3-free class N2D3P9".underline + NEWLINE +
+            "    /|\\ \t11M   \t33/32\t[   0   0   1 ⟩    \t 45.450\t -4.000      \t 11    \t 11          \t  6.722              " + NEWLINE +
+            "        \t25/49M\t50/49\t[   1   0   2  -2 ⟩\t 33.400\t -2.154      \t  7    \t 24          \t 26.466              " + NEWLINE as Io
         expect(actual).toEqual(expected)
     })
 
@@ -49,7 +49,7 @@ describe("computeFindCommasTable", () => {
 
         const expected =
             "[table]" + NEWLINE +
-            "[tr][th]symbol  [/th][th]name[/th][th]ratio[/th][th]monzo[/th][th]cents[/th][th]apotome slope[/th][th]limit[/th][th]5-rough sopfr[/th][th]N2D3P9[/th][/tr]" + NEWLINE +
+            "[tr][th]symbol  [/th][th]name[/th][th]ratio[/th][th]monzo[/th][th]cents[/th][th]apotome slope[/th][th]limit[/th][th]5-rough sopfr[/th][th]2,3-free class N2D3P9[/th][/tr]" + NEWLINE +
             "[tr][td]:/|\\:[/td][td]11M[/td][td]33/32[/td][td][   0   0   1 ⟩[/td][td] 45.450[/td][td] -4.000[/td][td] 11    [/td][td] 11    [/td][td]  6.722[/td][/tr]" + NEWLINE +
             "[tr][td][/td][td]25/49M[/td][td]50/49[/td][td][   1   0   2  -2 ⟩[/td][td] 33.400[/td][td] -2.154[/td][td]  7    [/td][td] 24    [/td][td] 26.466[/td][/tr]" + NEWLINE +
             "[/table]" + NEWLINE as Io
