3b41184278ef17c3b0084a997bcdbaf277e0c98d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-15 17:32:33 -0700

    forgot to use readlines everywhere it oculd be used

--- src/scripts/popularityMetricLfc/commands/shared/load.ts
+++ src/scripts/popularityMetricLfc/commands/shared/load.ts
@@ -1,17 +1,10 @@
-import * as fs from "fs"
-import { Filename, Io } from "../../../../general"
+import { Filename, Io, NEWLINE, readLines } from "../../../../general"
 import { unformatParameters } from "../../solver"
 
 const load = (filename: Filename): Object => {
-    return JSON.parse(
-        unformatParameters(
-            // TODO: this should use the new readLines method
-            fs.readFileSync(
-                `src/scripts/popularityMetricLfc/input/${filename}.txt`,
-                { encoding: "utf8" },
-            ) as Io,
-        ),
-    )
+    const fullFilename = `src/scripts/popularityMetricLfc/input/${filename}.txt` as Filename
+
+    return JSON.parse(unformatParameters(readLines(fullFilename).join(NEWLINE) as Io))
 }
 
 export {
