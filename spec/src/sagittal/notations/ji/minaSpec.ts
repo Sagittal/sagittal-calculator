9407c8ea172da38b416492269bb6e1c52ecf03a6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-16 19:08:35 -0700

    minas are keyed by symbol ids which is better

--- spec/src/sagittal/notations/ji/minaSpec.ts
+++ spec/src/sagittal/notations/ji/minaSpec.ts
@@ -1,2 +1,6 @@
 // TODO: a test for the values of those weird minas
 //  - and just a test for the order
+//  - okay actually the formula I was thinking of was for the boundaries next to these mina numbers
+//  - not the mina numbers themselves
+//  - I forget exactly how these mina numbers are calculated...
+
