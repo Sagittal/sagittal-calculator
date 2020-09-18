9b7360aa8ebc4c57d27b810ef9f6703f0b69b259
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-18 14:30:10 -0700

    test for prime limit

--- spec/src/general/music/ji/primeLimitSpec.ts
+++ spec/src/general/music/ji/primeLimitSpec.ts
@@ -1,6 +1,11 @@
-import { Min, Prime, Ratio } from "../../../../../src/general/math"
+import { Max, Min, Prime, Ratio } from "../../../../../src/general/math"
 import { Monzo } from "../../../../../src/general/math/monzo"
-import { computeIsWithinPrimeLimit, computeIsWithinPrimeMin, THREE_PRIME_LIMIT } from "../../../../../src/general/music"
+import {
+    computeIsWithinPrimeLimit,
+    computeIsWithinPrimeMin,
+    computePrimeLimit,
+    THREE_PRIME_LIMIT,
+} from "../../../../../src/general/music"
 import { FIVE_PRIME_LIMIT, SEVEN_PRIME_LIMIT } from "../../../../../src/general/music/ji/constants"
 
 describe("computeIsWithinPrimeLimit", (): void => {
@@ -51,7 +56,7 @@ describe("computeIsWithinPrimeMin", (): void => {
             expect(actual).toBeTruthy()
         })
 
-        it("returns false if the pitch is nhas no prime factors less than the prime min", (): void => {
+        it("returns false if the pitch has no prime factors less than the prime min", (): void => {
             const jiPitch = { monzo: [0, 0, 1] as Monzo }
 
             const actual = computeIsWithinPrimeMin(jiPitch, 7 as 7 & Min<Prime>)
@@ -69,7 +74,7 @@ describe("computeIsWithinPrimeMin", (): void => {
             expect(actual).toBeTruthy()
         })
 
-        it("returns false if the pitch is nhas no prime factors less than the prime min", (): void => {
+        it("returns false if the pitch has no prime factors less than the prime min", (): void => {
             const jiPitch = { ratio: [5, 4] as Ratio }
 
             const actual = computeIsWithinPrimeMin(jiPitch, 7 as 7 & Min<Prime>)
@@ -79,4 +84,23 @@ describe("computeIsWithinPrimeMin", (): void => {
     })
 })
 
-// TODO: test computePrimeLimit
+describe("computePrimeLimit", (): void => {
+    it("works for pitches with monzos", (): void => {
+        const jiPitch = { monzo: [0, 4, 1, -3, 0, 1] as Monzo }
+
+        const actual = computePrimeLimit(jiPitch)
+
+        const expected = 13 as Max<Prime>
+        expect(actual).toBe(expected)
+    })
+
+    it("works for pitches with ratios", (): void => {
+        const jiPitch = { ratio: [7, 5] as Ratio }
+
+        const actual = computePrimeLimit(jiPitch)
+
+        const expected = 7 as Max<Prime>
+        expect(actual).toBe(expected)
+    })
+})
+
