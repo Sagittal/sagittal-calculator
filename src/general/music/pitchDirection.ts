df2730f21fd3d9040fde87fef83c8cebddca4697
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-09-18 14:28:06 -0700

    handle pitch name in compute super pitch

--- src/general/music/pitchDirection.ts
+++ src/general/music/pitchDirection.ts
@@ -51,18 +51,13 @@ const computeSuperPitch = <T extends NumericTypeParameters, U extends Pitch<T>>(
 
     let superPitch: Pitch = {} as Pitch
     if (isSubPitch) {
-        const { monzo, ratio, cents } = pitch
+        const { monzo, ratio, cents, name } = pitch
         if (!isUndefined(ratio)) superPitch.ratio = computeSuperRatio(ratio)
         if (!isUndefined(monzo)) superPitch.monzo = computeSuperMonzo(monzo)
         if (!isUndefined(cents)) superPitch.cents = negative(cents)
-        // TODO: if this modifies name, then it must be moved into sagittal/
-        //  which I don't want to do if I don't have to
-        //  but on the other hand, if it doesn't flip the name, then this can make the name into a lie...
-        //  which makes me want to not allow name as a thing on a Pitch... but then you get into the e.g.
-        //  size category bounds which are named, along with the other types of snappable positions
-        //  but then those live between the scripts/jiNotationBound and the sagittal/ modules...
-        //  perhaps in this case it should just throw an error?
-        // if (!isUndefined(name)) superPitch.name = computeCommaName(superPitch as Comma)
+        if (!isUndefined(name)) {
+            throw new Error(`Pitch was named ${name} but was sub and taken the super of. Name should change.`)
+        }
     } else {
         superPitch = pitch
     }
