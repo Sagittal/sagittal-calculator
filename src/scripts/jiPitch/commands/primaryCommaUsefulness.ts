77bb970a11ce3f24dddcd4a8ae71ac92a5b423a7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-10-23 11:23:32 -0700

    a little work on primary comma usefulness

--- src/scripts/jiPitch/commands/primaryCommaUsefulness.ts
+++ src/scripts/jiPitch/commands/primaryCommaUsefulness.ts
@@ -1,9 +1,10 @@
-import {Comma, Id, saveLog, stringify} from "../../../general"
+import {Comma, compute23FreeClass, Id, saveLog, stringify} from "../../../general"
 import {
-    analyzeComma,
-    CommaAnalysis,
     CommaClass,
+    computeAas,
+    computeAte,
     computeCommaName,
+    computeN2D3P9,
     computeSecondaryCommaZone,
     formatCommaClass,
     getCommaClass,
@@ -21,6 +22,30 @@ applySharedPitchCommandSetup()
 
 const findCommasSettings = parseFindCommasSettings()
 
+// Vary these
+const DATE = 9 // The ATE at which N2D3P9 will get doubled.
+const DAAS = 9 // The AAS at which N2D3P9 will get doubled.
+
+// The two different forms
+/*
+Actually, I can see some serious floating-point overflow and underflow issues with the form:
+    N2D3P9 × 2^(2^(ATE-DATE)) × 2^(2^(AAS-DAAS))
+
+Can you build it into the code as:
+    lb(N2D3P9) + 2^(ATE-DATE) + 2^(AAS-DAAS)
+*/
+
+// Then for compress/expand
+
+// Either
+// Lb(n2d3p9) + 2^(ATE-DATE)
+// Or
+// N2d3p9^(1/2) * 2^2^???(ATE-DATE)^2 ???
+// Actually I'm not sure whether this is multiply or add, or if either of the 2^2^'s cancel
+
+// Finally the constants t and s
+// What are these
+
 JI_NOTATION.forEach((commaClassId: Id<CommaClass>): void => {
     const commaClass = getCommaClass(commaClassId)
     const commaName = computeCommaName(commaClass.pitch)
@@ -33,7 +58,25 @@ JI_NOTATION.forEach((commaClassId: Id<CommaClass>): void => {
 
     const commas = computeCommas({...jiPitchScriptGroupSettings, ...findCommasSettings, lowerBound, upperBound})
 
-    const commaAnalyses = commas.map((comma: Comma): CommaAnalysis => analyzeComma(comma))
+    // let bestUsefulness = Infinity
+    // let bestComma = undefined
+    commas.forEach((comma: Comma): void => {
+        const name = computeCommaName(comma)
+        const n2d3p9 = computeN2D3P9(compute23FreeClass(comma))
+        const aas = computeAas(comma)
+        const ate = computeAte(comma)
+        saveLog(stringify({name, n2d3p9, aas, ate}))
 
-    saveLog(stringify(commaAnalyses, {multiline: true}))
+        // const usefulness = log(n2d3p9, 2 as Base) + pow(ate - DATE, 2 as Exponent) + pow(aas - DAAS, 2 as Exponent)
+        // if (usefulness < bestUsefulness) {
+        //     bestUsefulness = usefulness
+        //     bestComma = comma
+        // }
+    })
+
+    // if (isUndefined(bestComma)) {
+    //     saveLog("No best comma found.")
+    // } else {
+    //     saveLog(stringify(analyzeComma(bestComma), {multiline: true}))
+    // }
 })
