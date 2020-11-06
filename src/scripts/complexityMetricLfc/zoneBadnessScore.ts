57f2299092b74629f21ce0287333318124578cdc
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-11-06 15:33:36 -0800

    sheesh fix ci

--- src/scripts/complexityMetricLfc/zoneBadnessScore.ts
+++ src/scripts/complexityMetricLfc/zoneBadnessScore.ts
@@ -38,7 +38,7 @@ const computeZoneBadnessScore = ([commaClassId, commas]: [CommaClassId, Comma[]]
         const isActualComma = areScamonsEqual(comma, actualComma)
 
         saveLog(
-            `${formatComma(comma)} badness: ${formatDecimal(badness)} ${isActualComma ? "* actual comma" : ""}`,
+            `${formatComma(comma)} badness: ${formatDecimal(badness)}${isActualComma ? "* actual comma" : ""}`,
             LogTarget.DETAILS,
         )
 
