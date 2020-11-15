ca5113b45b091daf0c245a14be381097a623bf8e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-11-14 17:22:21 -0800

    simplify conditionals

--- src/scripts/jiPitch/io/parse/pitch.ts
+++ src/scripts/jiPitch/io/parse/pitch.ts
@@ -9,12 +9,14 @@ import {
     IDENTIFYING_MONZO_CHARS,
     IDENTIFYING_QUOTIENT_CHARS,
     Io,
+    isUndefined,
+    Maybe,
     NUMERIC_CHARS,
     parseCents,
     parseDecimal,
     parseMonzo,
-    parseQuotient, saveLog,
-    Scamon, stringify,
+    parseQuotient,
+    Scamon,
 } from "../../../../general"
 import {
     Ascii,
@@ -26,37 +28,26 @@ import {
 import {PitchFormat} from "./types"
 
 const parsePitch = (pitchIo: Io, pitchFormat?: PitchFormat): Scamon => {
-    let pitch: Scamon
+    let pitch: Maybe<Scamon> = undefined
 
-    if (
-        pitchFormat === PitchFormat.COMMA_NAME
-        || (pitchIo.match(IDENTIFYING_COMMA_NAME_CHARS) && pitchIo.match(NUMERIC_CHARS))
-    ) {
-        const commaNameQuotientAndSizeCategoryName = parseCommaName(pitchIo)
-        pitch = computeCommaFromCommaNameQuotientAndSizeCategoryName(commaNameQuotientAndSizeCategoryName)
-    } else if (
-        pitchFormat === PitchFormat.QUOTIENT
-        || (pitchIo.match(IDENTIFYING_QUOTIENT_CHARS) && pitchIo.match(NUMERIC_CHARS))
-    ) {
-        const quotient = parseQuotient(pitchIo)
-        pitch = computeScamonFromQuotient(quotient)
-    } else if (
-        pitchFormat === PitchFormat.MONZO
-        || (pitchIo.match(IDENTIFYING_MONZO_CHARS) && pitchIo.match(NUMERIC_CHARS))
-    ) {
-        const monzo = parseMonzo(pitchIo)
-        pitch = computeScamonFromMonzo(monzo)
-    } else if (
-        pitchFormat === PitchFormat.CENTS
-        || (pitchIo.match(IDENTIFYING_CENTS_CHARS) && pitchIo.match(NUMERIC_CHARS))
-    ) {
-        const cents = parseCents(pitchIo)
-        pitch = computePitchFromCents(cents)
-    } else if (
-        pitchFormat === PitchFormat.ACCIDENTAL
-        // TODO: any way to simplify these NUMERIC_CHARS conditions
-        || (pitchIo.match(IDENTIFYING_ACCIDENTAL_CHARS) && !pitchIo.match(NUMERIC_CHARS))
-    ) {
+    if (pitchIo.match(NUMERIC_CHARS)) {
+        if (pitchFormat === PitchFormat.COMMA_NAME || pitchIo.match(IDENTIFYING_COMMA_NAME_CHARS)) {
+            const commaNameQuotientAndSizeCategoryName = parseCommaName(pitchIo)
+            pitch = computeCommaFromCommaNameQuotientAndSizeCategoryName(commaNameQuotientAndSizeCategoryName)
+        } else if (pitchFormat === PitchFormat.QUOTIENT || pitchIo.match(IDENTIFYING_QUOTIENT_CHARS)) {
+            const quotient = parseQuotient(pitchIo)
+            pitch = computeScamonFromQuotient(quotient)
+        } else if (pitchFormat === PitchFormat.MONZO || pitchIo.match(IDENTIFYING_MONZO_CHARS)) {
+            const monzo = parseMonzo(pitchIo)
+            pitch = computeScamonFromMonzo(monzo)
+        } else if (pitchFormat === PitchFormat.CENTS || pitchIo.match(IDENTIFYING_CENTS_CHARS)) {
+            const cents = parseCents(pitchIo)
+            pitch = computePitchFromCents(cents)
+        } else {
+            const decimal = parseDecimal(pitchIo)
+            pitch = computeScamonFromDecimal(decimal)
+        }
+    } else if (pitchFormat === PitchFormat.ACCIDENTAL || pitchIo.match(IDENTIFYING_ACCIDENTAL_CHARS)) {
         // TODO: perhaps this should be parseAccidental and it should take Io not Ascii, or both, or have a pass-thru
         const accidental = parseAscii(pitchIo as Ascii)
 
@@ -64,10 +55,9 @@ const parsePitch = (pitchIo: Io, pitchFormat?: PitchFormat): Scamon => {
         //  Er, well, at the moment, there is no way to get a non-JI pitch from an accidental
         //  One could imagine telling this thing which EDO you're in or something, and it tempering stuff
         pitch = computeJiPitchFromAccidental(accidental)
-    } else {
-        const decimal = parseDecimal(pitchIo)
-        pitch = computeScamonFromDecimal(decimal)
     }
+
+    if (isUndefined(pitch)) throw new Error(`Could not identify format of pitch ${pitchIo}`)
 
     return pitch
 }
