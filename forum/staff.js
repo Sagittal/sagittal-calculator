14b6fc53327000251e42cb6faa639c63112bcaff
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-12-01 12:04:11 -0800

    update explanation

--- forum/staff.js
+++ forum/staff.js
@@ -9,13 +9,17 @@ a4 nt4 c5 nt4 e5 nt4
 and see a chord displayed on a staff.
 
 This script will find all elements on the page matching the CSS selector `span.staff.unprocessed`
-and convert their `staffCode` from text to unicode. The Bravura Text font from Steinberg Media
-Technologies GmbH, designed by Daniel Spreadbury, is used to display as sheet music notation.
-For more information on Bravura Text, please see https://www.smufl.org/fonts/.
+and convert their `staffCode` from text to unicode. A modified version of the Bravura Text font
+from Steinberg MediaTechnologies GmbH, designed by Daniel Spreadbury, is used to display as sheet
+music notation. This project would not have been possible without the great work done on Bravura
+Text, and its precursors Bravura and SMuFL. For more information see https://www.smufl.org/fonts/.
 
 You may add an additional class to the span to indicate the clef. This will initiate a staff with
 said clef, and also cause the note position modifiers to adjust for that clef. If no clef class
 is provided, no clef will be displayed, and the note position modifiers will default to treble.
+
+staffCode assumes your site loads the BravuraTextBB font. We suggest you locate it in your
+assets/fonts folder. Please do not load it from another forum's assets, for your own performance.
 */
 
 (() => {
